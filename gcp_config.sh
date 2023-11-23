bb7d51e9ffd8cfe60b5060045b5b7c8682f49bce
theQuert <ga11004@cs.nccu.edu.tw>
2023-11-23 10:42:06 +0800

    Update config script

--- gcp_config.sh
+++ gcp_config.sh
@@ -17,3 +17,13 @@ sudo apt install -y python3-pip
 sudo pip3 install torch torchvision torchaudio
 # install venv
 sudo apt install python3.10-venv
+
+# reference
+# https://koding.work/how-to-install-cuda-and-cudnn-to-ubuntu-20-04/#Step_4_cuDNN
+# sudo apt install nvidia-cuda-toolkit
+# https://developer.nvidia.com/cuda-12-1-0-download-archive?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_local
+# https://developer.nvidia.com/rdp/cudnn-archive (according to CUDA version shown in nvidia-smi )
+# tar -Jxvf cudnn-linux-x86_64-8.9.5.30_cuda12-archive.tar.xz
+# sudo cp cuda/include/cudnn*.h /usr/local/cuda/include 
+# sudo cp -P cuda/lib/libcudnn* /usr/local/cuda/lib64
+# sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
