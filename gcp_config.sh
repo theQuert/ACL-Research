3bf604da530ebf1d8162ef2ca8d59ab80fa3f4c7
theQuert <ga11004@cs.nccu.edu.tw>
2023-12-08 11:56:03 +0800

    Update init on vm

--- gcp_config.sh
+++ gcp_config.sh
@@ -3,27 +3,33 @@ sudo apt autoremove -y
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.10 -y
-# install cuda version 11.7
+
+# install cuda version 11.6
+sudo apt-get --purge remove cuda
+
+# https://developer.nvidia.com/cuda-11-6-0-download-archive
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
+wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.0-510.39.01-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
 sudo apt-get update
 sudo apt-get -y install cuda
 sudo apt -y install nvidia-cuda-toolkit
+
+# vim ./venv/bin/activate, and add the following contexts to tail of the file
+export PATH=/usr/local/cuda-11.6/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.6/lib64:$LD_LIBRARY_PATH
+
+# check installation
+sudo reboot
+nvcc -V
+nvidia-smi
+# CUDA is followed with `nvidia-smi`
+
+
 # prepare torch environment
 sudo apt install -y python3-pip
-sudo pip3 install torch torchvision torchaudio
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
 # install venv
 sudo apt install python3.10-venv
-
-# reference
-# https://koding.work/how-to-install-cuda-and-cudnn-to-ubuntu-20-04/#Step_4_cuDNN
-# sudo apt install nvidia-cuda-toolkit
-# https://developer.nvidia.com/cuda-12-1-0-download-archive?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_local
-# https://developer.nvidia.com/rdp/cudnn-archive (according to CUDA version shown in nvidia-smi )
-# tar -Jxvf cudnn-linux-x86_64-8.9.5.30_cuda12-archive.tar.xz
-# sudo cp cuda/include/cudnn*.h /usr/local/cuda/include 
-# sudo cp -P cuda/lib/libcudnn* /usr/local/cuda/lib64
-# sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
